a79c6c2bd303a9edbee418f96637c18769df538f
Joe Vogel <joejvogel@gmail.com>
2025-09-17 20:29:05 -0300

    Indentação do arquivo db.js

--- src/db.js
+++ src/db.js
@@ -12,27 +12,30 @@ const db = new sqlite3.Database(dbPath);
 // auctions(id, name, starting_price)
 // bids(id, auction_id, user_id, amount)
 db.serialize(() => {
-db.run(`CREATE TABLE IF NOT EXISTS users (
-id INTEGER PRIMARY KEY,
-name TEXT NOT NULL
-)`);
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+        )`
+    );
 
 
-db.run(`CREATE TABLE IF NOT EXISTS auctions (
-id INTEGER PRIMARY KEY,
-name TEXT NOT NULL,
-starting_price REAL NOT NULL
-)`);
+    db.run(`CREATE TABLE IF NOT EXISTS auctions (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        starting_price REAL NOT NULL
+        )`
+    );
 
 
-db.run(`CREATE TABLE IF NOT EXISTS bids (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-auction_id INTEGER NOT NULL,
-user_id INTEGER NOT NULL,
-amount REAL NOT NULL,
-FOREIGN KEY (auction_id) REFERENCES auctions(id),
-FOREIGN KEY (user_id) REFERENCES users(id)
-)`);
+    db.run(`CREATE TABLE IF NOT EXISTS bids (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        auction_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        amount REAL NOT NULL,
+        FOREIGN KEY (auction_id) REFERENCES auctions(id),
+        FOREIGN KEY (user_id) REFERENCES users(id)
+        )`
+    );
 });
 
 
